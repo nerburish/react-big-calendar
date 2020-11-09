d3c6aa35a7eeea82e9c0c7fe6a6167ec2f27cd48
Unknown <nerburish@gmail.com>
2020-11-09 16:27:42 +0100

    Subido build

--- src/Month.js
+++ src/Month.js
@@ -200,7 +200,7 @@ class MonthView extends React.Component {
 
     return (
       <Overlay
-        rootClose
+        rootClose={false}
         placement="bottom"
         show={!!overlay.position}
         onHide={() => this.setState({ overlay: null })}
